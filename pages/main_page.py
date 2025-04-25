eadfbd43167d7d975c50a4790fe3233e6ce670e9
Hamza Ali <hamzamughal2145@gmail.com>
2025-04-25 15:46:10 -0400

    testing cross browsers

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from selenium.webdriver.common.by import By
 
 from pages.base_page import Page
@@ -5,10 +7,12 @@ from pages.base_page import Page
 
 class MainPage(Page):
     EMAIL_BOX = (By.ID, 'email-2')
+    OFF_PLAN_SIDE_BTN = (By.CSS_SELECTOR, "[class*='menu-button'][href*='off-plan']")
 
     def open(self):
         self.open_url(f"{self.base_url}/sign-in")
         self.wait_until_clickable(*self.EMAIL_BOX)
 
     def click_off_plan_from_side(self):
-        self.click(By.CSS_SELECTOR, "[class*='menu-button'][href*='off-plan']")
+        self.click(*self.OFF_PLAN_SIDE_BTN)
+        sleep(2)
